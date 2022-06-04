fcbf0a583332866752b06457d21826c4f9933789
victor luiz <100322056+VICTORLUIZALMEIDAMEDEIROS@users.noreply.github.com>
2022-06-03 23:48:53 -0300

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,17 @@
+package testecursogames;
+
+public class game
+public static void mai(String[] args) {
+	
+	int contador = 0;
+	
+	while(contador <10) {
+		
+		System.out.println("o numero atual é: "+ cotnador);
+		
+			contador++;
+	}
+	
+	}
+
+}
