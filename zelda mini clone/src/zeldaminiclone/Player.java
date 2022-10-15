2b1c4ba05e36fd4f2e83ca381862e3ce8a683eeb
victor luiz <100322056+VICTORLUIZALMEIDAMEDEIROS@users.noreply.github.com>
2022-10-15 19:14:19 -0300

    Add files via upload

--- zelda mini clone/src/zeldaminiclone/Player.java
+++ zelda mini clone/src/zeldaminiclone/Player.java
@@ -3,6 +3,8 @@ package zeldaminiclone;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player extends Rectangle {
 	
@@ -13,6 +15,12 @@ public class Player extends Rectangle {
 	
 	public int curFrames = 0, targetFrames = 15;
 	
+	public static List<Bullet> bullets = new ArrayList<Bullet>();
+	
+	public boolean shoot = false;
+	
+	public int dir = 1;
+	
 	public Player(int x, int y) {
 		super(x,y,32,32);
 	}
@@ -22,9 +30,11 @@ public class Player extends Rectangle {
 		if(right && World.isFree(x+spd, y)) {
 			x+=spd;
 			moved = true;
+			dir = 1;
 		}else if(left && World.isFree(x-spd, y)) {
 			x-=spd;
 			moved = true;
+			dir = -1;
 		}
 		if(up && World.isFree(x, y-spd)) {
 			y-=spd;
@@ -43,11 +53,24 @@ public class Player extends Rectangle {
 			}
 		}
 		}
+		if (shoot) {
+			shoot = false;
+			bullets.add(new Bullet(x,y));
+		}
+		for(int i = 0; i < bullets.size(); i++) {
+			bullets.get(i).tick();
+			
+			
+		}
 	}
 	
 	
 	public void render(Graphics g) {
 		g.drawImage(Spritesheet.player_front[curAnimation],x,y,32,32, null);
-	}
+		
+		for(int i = 0; i < bullets.size(); i++) {
+			bullets.get(i).render(g);
+		}
 
+}
 }
